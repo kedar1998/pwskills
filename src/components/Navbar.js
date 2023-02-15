b8b40551035c7eb261d766dc1a8eae7ee86ce91b
Kedar <kedarmakode1598@gmail.com>
2023-02-15 19:46:16 +0530

    add link to login screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from '../assets/logo.png'
-
+import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='my-5'>
@@ -23,8 +23,8 @@ const Navbar = () => {
                         <li><a href="https://pwskills.com/">Job Portal</a></li>
                         <li><a href="https://pwskills.com/">Experience Portal</a></li>
                         <li><a href="https://pwskills.com/">More</a></li>
-                        <li><a href="https://account.pwskills.com/signin?domain=pwskills.com&redirectUrl=%2F" className='bg-[#5A4BDA] px-4 py-2 text-white font-semi
-                        '>Login / Sign up</a></li>
+                        <li><Link to="login" className='bg-[#5A4BDA] px-4 py-2 text-white font-semi
+                        '>Login / Sign up</Link></li>
                     </ul>
                 </div>
             </nav>
